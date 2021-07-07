64a836ff863012ec2cb11480cedb7869b6615ad5
devshine2015 <nikita.afanasev2020@gmail.com>
2021-07-07 15:47:38 +0300

    modify router to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import Routes from './routes';
-import Navbar from './components/NavbarComponent';
+import React from "react";
+import { HashRouter as Router } from "react-router-dom";
+import Routes from "./routes";
+import Navbar from "./components/NavbarComponent";
 
 // Redux
-import { Provider } from 'react-redux';
+import { Provider } from "react-redux";
 
-import configureStore from './store';
-import history from './utils/history';
+import configureStore from "./store";
+import history from "./utils/history";
 
-import './App.css';
-import 'antd/dist/antd.css';
+import "./App.css";
+import "antd/dist/antd.css";
 
 const initialState = {};
 const store = configureStore(initialState, history);
 
 const App = () => {
-	return (
-		<Provider store={store}>
-			<Router>
-				<Routes />
-			</Router>
-		</Provider>
-	);
+  return (
+    <Provider store={store}>
+      <Router>
+        <Routes />
+      </Router>
+    </Provider>
+  );
 };
 
 export default App;
